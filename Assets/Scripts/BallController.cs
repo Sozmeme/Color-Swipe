a328065ca60ccb5e6ff67e412c0cecff1d7bdff5
Daniil Smirnov <dsmir1101@gmail.com>
2023-05-23 19:35:03 +0300

    Added ball and camera controllers

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public float speed = 5f; // скорость движения шара
+    public float hspeed = 5f; // скорость движения шара
+    public float vspeed = 1.0f;
     private Rigidbody rb;
 
     private void Start()
@@ -16,7 +17,7 @@ public class BallController : MonoBehaviour
     {
         // управляем движением шара по горизонтали
         float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 horizontalVelocity = new Vector3(horizontalInput, 0f, 0f) * speed;
-        rb.velocity = new Vector3(horizontalVelocity.x, rb.velocity.y, horizontalVelocity.z);
+        Vector3 movement = new Vector3(horizontalInput * hspeed, rb.velocity.y, vspeed);
+        rb.AddForce(movement);
     }
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 //Isn't using currently
-public class FollowPlayer : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     public GameObject Player;
     private Vector3 offset;
@@ -21,13 +21,13 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 targetPos = Player.transform.localPosition - offset;
-        targetPos.x = 0f;
+
+        Vector3 targetPos = new Vector3(transform.localPosition.x, transform.localPosition.y, Player.transform.localPosition.z - offset.z);
         transform.localPosition = targetPos;
 
-        Quaternion targetRoatiton = Quaternion.Euler(27f, Player.transform.eulerAngles.y, 0f);
+        /*Quaternion targetRoatiton = Quaternion.Euler(27f, Player.transform.eulerAngles.y, 0f);
         Debug.Log(targetRoatiton.eulerAngles.ToString());
-        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRoatiton, rLerp);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRoatiton, rLerp);*/
 
         /*transform.position = Player.transform.position + offset;
         transform.LookAt(Player.transform.position);*/
